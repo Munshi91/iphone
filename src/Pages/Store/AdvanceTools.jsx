2dab3700123cb5b63a161e7545fc2859a12f0823
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2024-08-04 12:59:41 +0600

    Create AdvanceTools.jsx

---
+++ src/Pages/Store/AdvanceTools.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AdvanceTools = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <h2>Advanced tools for advanced studies.</h2>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default AdvanceTools;

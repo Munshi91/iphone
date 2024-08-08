93ade0754e479835d0ff8384a30439be60d9cc05
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2024-08-08 23:48:17 +0600

    Create AboutUs.jsx

---
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <>
+      <div className="max-w-7xl mx-auto text-black my-4 md:my-10 relative">
+        <h2 className="text-5xl font-bold text-center py-4">About Us</h2>
+        <p className="text-center">
+          Welcome to Education Doorway, one of the leading service providers for
+          students. <br /> We are on a mission to fulfill students' potential by
+          connecting to world-leading education.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutUs;

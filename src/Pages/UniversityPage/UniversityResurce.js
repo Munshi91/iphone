c1fc37fe5339437f6248530dfa2c833b22a0d222
Munshi91 <mimunshi91@gmail.com>
2024-08-22 17:41:13 +0600

    Create UniversityResurce.js

---
+++ src/Pages/UniversityPage/UniversityResurce.js
@@ -0,0 +1,11 @@
+import HU_Image from '../../../public/assets/images/University-of-Hertfordshire-Campus-July2020.webp';
+import HU_Image_2 from '../../../public/assets/images/University-of-Hertfordshire-Campus-Aerial-July2020.webp';
+import HU_Image_3 from '../../../public/assets/images/University-of-Hertfordshire-Pool-July2020.webp';
+import HU_Image_4 from '../../../public/assets/images/University-of-Hertfordshire-Science-Building-July2020.webp';
+import location from '../../../public/assets/images/location.png';
+
+export const Image_1 = HU_Image;
+export const Image_2 = HU_Image_2;
+export const Image_3 = HU_Image_3;
+export const Image_4 = HU_Image_4;
+export const Location = location;

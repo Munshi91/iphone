173105ec9475190ee847ac1e609cf1107bb530be
Munshi91 <54263790+Munshi91@users.noreply.github.com>
2024-08-26 14:29:15 +0600

    Update UniversityResurce.js

--- src/Pages/UniversityPage/UniversityResurce.js
+++ src/Pages/UniversityPage/UniversityResurce.js
@@ -4,6 +4,7 @@ import HU_Image_3 from '../../../public/assets/images/University-of-Hertfordshir
 import HU_Image_4 from '../../../public/assets/images/University-of-Hertfordshire-Science-Building-July2020.webp';
 import location from '../../../public/assets/images/location.png';
 import ukFlag from '../../../public/assets/images/united-kingdom.png';
+import Icon from '../../../public/assets/images/Login Icon.png';
 
 export const Image_1 = HU_Image;
 export const Image_2 = HU_Image_2;
@@ -11,3 +12,4 @@ export const Image_3 = HU_Image_3;
 export const Image_4 = HU_Image_4;
 export const Location = location;
 export const UK = ukFlag;
+export const Login = Icon;
